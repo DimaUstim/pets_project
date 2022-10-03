2e4365d3e68f21d63ee01c260c4626876ddff1c4
Ustimov Dmitriy <hccctt@gmail.com>
2022-10-03 20:29:04 +0300

    added 3 pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PetsComponent } from './pages/pets/pets.component';
+import { SingInComponent } from './pages/sing-in/sing-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    PetsComponent,
+    SingInComponent
   ],
   imports: [
     BrowserModule,
