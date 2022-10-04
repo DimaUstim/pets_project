dffdd9e3839c8d4cb715bcc6c0f0dae6c63d0e6c
Ustimov Dmitriy <hccctt@gmail.com>
2022-10-04 15:07:44 +0300

    added router and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MydashboardComponent } from './pages/mydashboard/mydashboard.component';
+import { PetFormComponent } from './pages/pet-form/pet-form.component';
+import { PetsComponent } from './pages/pets/pets.component';
+import { SingInComponent } from './pages/sing-in/sing-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'petFormFound',
+    component: PetFormComponent,
+  },
+  {
+    path: 'petFormLost',
+    component: PetFormComponent,
+  },
+  {
+    path: 'pets',
+    component: PetsComponent,
+  },
+  {
+    path: 'myDashboard',
+    component: MydashboardComponent,
+  },
+  {
+    path: 'singIn',
+    component: SingInComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
