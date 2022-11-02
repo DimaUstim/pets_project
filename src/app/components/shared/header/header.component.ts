c17ec7c92fb0a20934bff6f299c1d932c6573777
Ustimov Dmitriy <hccctt@gmail.com>
2022-11-02 19:25:47 +0200

    Add change to header component

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -15,46 +15,40 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   items: MenuItem[] = [];
   isLoggedIn: boolean = false;
-  signInMenuItem: MenuItem = {
-    label: 'Sign In', icon: 'pi pi-fw pi-sign-in', command: () => {
-      this.dialog.open(SignInComponent);
+  signInMenuItem!: MenuItem;
+  logOutMenuItem!: MenuItem;
+
+  constructor(private dialog: OverlayService, private signInService: SignInService, private route: Router) {
+    this.initMenuItems();
+  }
+
+  initMenuItems() {
+    this.signInMenuItem = {
+      label: 'Sign in',
+      icon: 'pi pi-fw pi-sign-in',
+      command: () => {
+        this.dialog.open(SignInComponent);
+      },
     }
-  }
 
-  logOutMenuItem: MenuItem = {
-    label: 'Log out',
-    icon: 'pi pi-fw pi-sign-out',
-    command: () => {
-      const dialogRef = this.dialog.open(PopUpComponent, {
-        data: {
-          title: 'Do you want log out?',
-        },
-        backdropClickClose: true,
-      });
-      dialogRef.afterClosed().subscribe((result) => {
-        if (result) {
-          this.signInService.logout();
-          this.route.navigate(['']);
-        }
-      });
-    },
-  }
-
-  constructor(private dialog: OverlayService, private signInService: SignInService, private route: Router) { }
-
-  ngOnInit(): void {
-    this.signInService.userChanged.subscribe((user) => {
-      this.isLoggedIn = user ? true : false;
-      this.items = Array.from(this.items);
-      if (this.isLoggedIn) {
-        this.items.pop();
-        this.items.push(this.logOutMenuItem);
-      }
-      if (!this.isLoggedIn) {
-        this.items.pop();
-        this.items.push(this.signInMenuItem);
-      }
-    });
+    this.logOutMenuItem = {
+      label: 'Log out',
+      icon: 'pi pi-fw pi-sign-out',
+      command: () => {
+        const dialogRef = this.dialog.open(PopUpComponent, {
+          data: {
+            title: 'Do you want to log out?',
+          },
+          backdropClickClose: true,
+        });
+        dialogRef.afterClosed().subscribe((result) => {
+          if (result) {
+            this.signInService.logout();
+            this.route.navigate(['']);
+          }
+        });
+      },
+    }
 
     this.items = [
       {
@@ -81,6 +75,19 @@ export class HeaderComponent implements OnInit {
       this.signInMenuItem,
     ];
   }
+
+  ngOnInit(): void {
+    this.signInService.userChanged.subscribe((user) => {
+      this.isLoggedIn = user ? true : false;
+      this.items = Array.from(this.items);
+      if (this.isLoggedIn) {
+        this.items.pop();
+        this.items.push(this.logOutMenuItem);
+      }
+      if (!this.isLoggedIn) {
+        this.items.pop();
+        this.items.push(this.signInMenuItem);
+      }
+    });
+  }
 }
-
-
